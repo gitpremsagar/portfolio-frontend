db21b60280bdb27481aeb69aa2c4eb27297040f5
gitpremsagar <psagar172@gmail.com>
2024-05-21 23:54:57 +0530

    style - Show Hero image on Hero section

--- src/components/homepage/heroSection/HeroSection.tsx
+++ src/components/homepage/heroSection/HeroSection.tsx
@@ -3,11 +3,18 @@ import Section from "@/components/customUIs/Section";
 const HeroSection: React.FC = () => {
   return (
     <Section>
-      <div className="flex flex-col items-center justify-center h-full bg-gray-700 text-white p-20">
-        <h1 className="text-4xl font-bold text-center">I'm Prem Sagar</h1>
-        <p className="text-center text-lg mt-4">
-          I'm a full-stack developer and a freelance web developer
-        </p>
+      <div
+        className={`flex flex-col items-enter justify-end h-screen bg-[url("/me6-min.jpg")] bg-cover bg-center bg-no-repeat`}
+      >
+        <div className="bg-black bg-opacity-50 p-20 text-white -translate-y-40 text-center">
+          <h1 className="font-bold text-6xl">Hi, I'm Prem Sagar.</h1>
+          <p className="text-xl ">
+            I'm a full-stack developer, a YouTuber and a teacher and a loving
+            father of my daughter. I like to work as freelancer. Technologies
+            that i use when I create website are React, Node.js, MongoDB, HTML,
+            JavaScript and more.
+          </p>
+        </div>
       </div>
     </Section>
   );
